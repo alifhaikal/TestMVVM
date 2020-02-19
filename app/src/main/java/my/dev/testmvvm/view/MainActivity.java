fb8ce18dcfccbff111df0a1edc96ab8cc496a2af
alifhaikal <ahaikal.ah26@gmail.com>
2020-02-20 00:52:41 +0800

    Insert binding data and MVVM architecture

--- app/src/main/java/my/dev/testmvvm/view/MainActivity.java
+++ app/src/main/java/my/dev/testmvvm/view/MainActivity.java
@@ -1,16 +1,30 @@
 package my.dev.testmvvm.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 
 import my.dev.testmvvm.R;
+import my.dev.testmvvm.databinding.ActivityMainBinding;
+import my.dev.testmvvm.viewmodel.LoginViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
     }
 }
